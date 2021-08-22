0f2694ac6c44c50994c6c21e6931b1383059494d
prashant169 <prashantphad2018@gmail.com>
2021-08-22 00:36:53 -0700

    calculating for parttime employee

--- main.sh
+++ main.sh
@@ -1,15 +1,22 @@
 #! /bin/bash -x
 
 wagePerHr=20
-fullday=8
+IS_FULLTIME=8
+IS_PARTTIME=4
 
-ranFun=$((RANDOM%2))
+ranFun=$((RANDOM%3))
 
 if [[ $ranFun -eq 1 ]]
 then 
-    echo "Prasent"
-	 dailywage=$(( $wagePerHr * $fullday ))
+    echo "Employee is fulltime"
+	 dailywage=$(( $wagePerHr * $IS_FULLTIME ))
     echo $dailywage
+elif [[ $ranFun -eq 2 ]]
+then
+	 echo "Employee is parttime"
+	 dailywage=$(( $wagePerHr * $IS_PARTTIME ))
+	 echo $dailywage
+
 else
-	echo "Absent"
+	 echo "Absent"
 fi
